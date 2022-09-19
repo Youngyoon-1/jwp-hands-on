cfe3627c6898cd0e528ae264989a038c27a99075
yoon <duddbs774@gmail.com>
2022-09-20 01:02:01 +0900

    feat: Servlet, Reflection 테스트 완료

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -14,5 +18,11 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+        typesAnnotatedWith.forEach(it -> log.info(it.toString()));
+        final Set<Class<?>> typesAnnotatedWith1 = reflections.getTypesAnnotatedWith(Service.class);
+        typesAnnotatedWith1.forEach(it -> log.info(it.toString()));
+        final Set<Class<?>> typesAnnotatedWith2 = reflections.getTypesAnnotatedWith(Repository.class);
+        typesAnnotatedWith2.forEach(it -> log.info(it.toString()));
     }
 }
